14ea4b13f3349829c2955838a5feb470f29ca193
CoderX <66735676+ToxygenX@users.noreply.github.com>
2024-03-10 19:54:40 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-REPO="https://github.com/TeamUltroid/Ultroid.git"
+REPO="https://github.com/spacexmeowser/Ultroid.git"
 CURRENT_DIR="$(pwd)"
 ENV_FILE_PATH=".env"
-DIR="/root/TeamUltroid"
+DIR="/root/spacexmeowser"
 
 while [ $# -gt 0 ]; do
     case "$1" in
     --dir=*)
-        DIR="${1#*=}" || DIR="/root/TeamUltroid"
+        DIR="${1#*=}" || DIR="/root/spacexmeowser"
         ;;
     --branch=*)
         BRANCH="${1#*=}" || BRANCH="main"
